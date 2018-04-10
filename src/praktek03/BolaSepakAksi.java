c55a874f340d59fb4667e91e12ed307882487474
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2018-04-10 16:18:00 +0800

    Instance,buat attribut dan panggil method

--- src/praktek03/BolaSepakAksi.java
+++ src/praktek03/BolaSepakAksi.java
@@ -3,6 +3,14 @@ package praktek03;
 public class BolaSepakAksi {
     public static void main(String[] args) {
         
+        BolaSepak bs = new BolaSepak();
+        bs.merk="Adidas";
+        bs.warna="Hijau";
+        bs.harga=1000;
+        
+        bs.cetakInfo();
+        
     }
     
 }
+
