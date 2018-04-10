fdaeb0774d0c9c8727629a511b94df415464f90b
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2018-04-10 16:28:51 +0800

    Menggunakan method set

--- src/praktek03/BolaSepakAksi.java
+++ src/praktek03/BolaSepakAksi.java
@@ -4,9 +4,9 @@ public class BolaSepakAksi {
     public static void main(String[] args) {
         
         BolaSepak bs = new BolaSepak();
-        bs.merk="Adidas";
-        bs.warna="Hijau";
-        bs.harga=1000;
+        bs.setMerk("Adidas");
+        bs.setWarna("Merah");
+        bs.setHarga(1000);
         
         bs.cetakInfo();
         
