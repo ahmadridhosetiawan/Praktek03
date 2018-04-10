dd54850e589f385257f90b250e157d6f08e3ef79
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2018-04-10 16:45:23 +0800

    attribut dan method

--- src/praktek03/BolaVoly.java
+++ src/praktek03/BolaVoly.java
@@ -3,5 +3,9 @@ package praktek03;
 
 
 public class BolaVoly extends BolaSepak{
+    private String JenisBolaVoly;
     
+    void throttle(){
+        System.out.println("Dabuk Dabuk.....");
+    }
 }
