2c86600ecbf2c1be4b013a041691163c0500c00f
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2018-04-10 16:36:20 +0800

    Menggunakan method get

--- src/praktek03/BolaSepakAksi.java
+++ src/praktek03/BolaSepakAksi.java
@@ -9,6 +9,12 @@ public class BolaSepakAksi {
         bs.setHarga(1000);
         
         bs.cetakInfo();
+        System.out.print("Merknya \t:");
+        System.out.println(bs.getMerk());
+        System.out.print("Warnanya \t:");
+        System.out.println(bs.getWarna());
+        System.out.print("Harganya \t:");
+        System.out.println(bs.getHarga());
         
     }
     
